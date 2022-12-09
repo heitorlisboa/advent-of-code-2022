d1afc6497f345f6c723708c1790a43d47065515f
heitorlisboa <heitor01101000@gmail.com>
2022-12-09 19:04:53 -0300

    Day 5

---
+++ 5/b.ts
@@ -0,0 +1,53 @@
+const input = await Deno.readTextFile('./input.txt');
+
+const [startingStacks, procedureSteps] = input.split('\n\n');
+
+function parseStacks(stacks: string) {
+  const stackRows = stacks.split('\n').slice(0, -1).reverse();
+  // Size of the bottom row of stacks
+  const numberOfStacks = stackRows[0].length + 1 / 4;
+  const parsedStacks: string[][] = [];
+
+  for (let currentStack = 0; currentStack < numberOfStacks; currentStack++) {
+    const crateLetterIndex = currentStack * 4 + 1;
+    parsedStacks.push(
+      stackRows
+        // Iterating over the rows getting all the crates of the current stack
+        .map((stackRow) => stackRow[crateLetterIndex])
+        // Removing the empty strings
+        .filter((crate) => crate !== ' ')
+    );
+  }
+
+  return parsedStacks;
+}
+
+function rearrangeStacks(stacks: string[][]) {
+  const stacksCopy = stacks.map((stack) => [...stack]);
+
+  for (const procedureStep of procedureSteps.split('\n')) {
+    const [numberOfCratesToMove, fromStack, toStack] = procedureStep
+      .match(/move (\d+) from (\d) to (\d)/)!
+      // Removing the match string
+      .slice(1)
+      .map((string) => parseInt(string));
+
+    const removedCrates = stacksCopy[fromStack - 1].splice(
+      stacksCopy[fromStack - 1].length - numberOfCratesToMove,
+      numberOfCratesToMove
+    );
+
+    stacksCopy[toStack - 1].push(...removedCrates);
+  }
+
+  return stacksCopy;
+}
+
+const parsedStacks = parseStacks(startingStacks);
+const stacksAfterRearrangement = rearrangeStacks(parsedStacks);
+
+const cratesOnTop = stacksAfterRearrangement
+  .map((stack) => stack.at(-1)!)
+  .join('');
+
+console.log(cratesOnTop);
